9ed756ffe926311f211964dfe208deb05356e6ed
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-06 13:03:31 +1100

    can add Xs and Ox and take turns

--- js/main.js
+++ js/main.js
@@ -1,1 +1,60 @@
-console.log( `JS sees you` );
+
+
+
+
+
+$(document).ready(function () {
+  // create flag for which turn, and initialise to start with X
+  let xTurn = true;
+  // get number of cells
+  const numCells = $('.xo').length;
+
+
+
+  // ******** POPULATE CELL FUNCTION ************
+  const populateCell = function ( cellNum ) {
+    // if cell is used, exit
+    if ($('.xo').eq( cellNum ).text()) {
+      console.log( `That cell is taken` );
+      return;
+    }
+    // else
+    if (xTurn) { // put an X
+      $('.xo').eq( cellNum ).text('X');
+      xTurn = false;
+    } else { // put a O
+      $('.xo').eq( cellNum ).text('O');
+      xTurn = true;
+    }
+  };
+
+
+  // *********** EVENT HANDLERS *************
+  for (let i = 0; i < numCells; i++) {
+    $('.cell').eq(i).on( 'click', populateCell.bind(null, i) );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});  // end document ready
