1f5da84fd68178bc9476d11c79cdfb7cb93249e0
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-09 19:01:32 +1100

    commented out logging

--- js/ai.js
+++ js/ai.js
@@ -21,7 +21,7 @@ const ai = {
   },
 
   cellIsFree: function ( n ) {
-    console.log( `Inside cellIsFree for n ${n} >>${ $('.xo').eq( n ).text() }<<` );
+    // console.log( `Inside cellIsFree for n ${n} >>${ $('.xo').eq( n ).text() }<<` );
     if ( $('.xo').eq( n ).text() === '') {
       return true; // learnt not to return n, cos 0 is falsey
     }
@@ -42,7 +42,7 @@ const ai = {
     return false;
   },
 
-  checkForPossWin: function () {  // I.E. CHECKING WHERE TO BLOCK X FROM WIN
+  checkForPossWin: function () {  // I.E. !!!! CHECKING WHERE TO BLOCK X FROM WIN
     let blockCell;
 
     // for each cell number
@@ -50,34 +50,33 @@ const ai = {
 
         // if that cell number is taken, skip to next one
         if ( !this.cellIsFree( i ) ) {
-          console.log( `${i} cell is taken` );
+          // console.log( `${i} cell is taken` );
           continue;
         }
-        console.log( `******************** Pretending cell ${i} is an x...` );
+        // console.log( `******************** Pretending cell ${i} is an x...` );
 
         // if it's blank, let's run the win tests if that cell is an X
         $('.xo').eq( i ).addClass('hidden');
         $('.xo').eq( i ).text('x');
-        console.log( `Now in cell ${i}: ` + $('.xo').eq( i ).text() );
+        // console.log( `Now in cell ${i}: ` + $('.xo').eq( i ).text() );
 
         // check ROW for win
         for (let r = 0; r < numRows; r++) {
-          console.log( `Examining row number ${r} and i/fake x is ${i}` );
+          // console.log( `Examining row number ${r} and i/fake x is ${i}` );
           for (let j = 0; j < numRows; j++) {
             let cn = numRows * r + j; // cell num
-            console.log( `Examining cell ${ cn } and i/fake x is ${i}` );
-            // debugger;
-            console.log( `First condition - is an O:  ${this.cellIsO( cn )}` );
-            console.log( `Second condition - is blank:  ${this.cellIsFree( cn )}` );
-            console.log( `NOW IN CELL i ${i} BEFORE CHECK: ` + $('.xo').eq( i ).text() );
-            console.log( `NOW IN CELL cn ${cn} BEFORE CHECK: ` + $('.xo').eq( cn ).text() );
+            // console.log( `Examining cell ${ cn } and i/fake x is ${i}` );
+            // console.log( `First condition - is an O:  ${this.cellIsO( cn )}` );
+            // console.log( `Second condition - is blank:  ${this.cellIsFree( cn )}` );
+            // console.log( `NOW IN CELL i ${i} BEFORE CHECK: ` + $('.xo').eq( i ).text() );
+            // console.log( `NOW IN CELL cn ${cn} BEFORE CHECK: ` + $('.xo').eq( cn ).text() );
             if ( this.cellIsO( cn ) || this.cellIsFree( cn ) ) { // i.e. it is not a win combo for x
-              console.log( `Not an impending win combo` );
+              // console.log( `Not an impending win combo` );
               break; // break out of J LOOP, start to check a new row
             }
             if (j === numRows - 1) { // i.e. we've finished iterating and it didn't break so it must be a line with only x's
               blockCell = i;
-              console.log( `blockCell = ${blockCell}` );
+              // console.log( `blockCell = ${blockCell}` );
               // remove simulation
               $('.xo').eq( i ).text('');
               $('.xo').eq( i ).removeClass('hidden');
@@ -90,22 +89,22 @@ const ai = {
 
         // check COLUMN for win
         for (let cl = 0; cl < numRows; cl++) {
-          console.log( `Examining col number ${cl} and i/fake x is ${i}` );
+          // console.log( `Examining col number ${cl} and i/fake x is ${i}` );
           for (let j = 0; j < numRows; j++) {
             let cn = numRows * j + cl; // cell num
-            console.log( `Examining cell ${ cn } and i/fake x is ${i}` );
+            // console.log( `Examining cell ${ cn } and i/fake x is ${i}` );
             // debugger;
-            console.log( `First condition - is an O:  ${this.cellIsO( cn )}` );
-            console.log( `Second condition - is blank:  ${this.cellIsFree( cn )}` );
-            console.log( `NOW IN CELL i ${i} BEFORE CHECK: ` + $('.xo').eq( i ).text() );
-            console.log( `NOW IN CELL cn ${cn} BEFORE CHECK: ` + $('.xo').eq( cn ).text() );
+            // console.log( `First condition - is an O:  ${this.cellIsO( cn )}` );
+            // console.log( `Second condition - is blank:  ${this.cellIsFree( cn )}` );
+            // console.log( `NOW IN CELL i ${i} BEFORE CHECK: ` + $('.xo').eq( i ).text() );
+            // console.log( `NOW IN CELL cn ${cn} BEFORE CHECK: ` + $('.xo').eq( cn ).text() );
             if ( this.cellIsO( cn ) || this.cellIsFree( cn ) ) { // i.e. it is not a win combo for x
-              console.log( `Not an impending win combo` );
+              // console.log( `Not an impending win combo` );
               break; // break out of J LOOP, start to check a new row
             }
             if (j === numRows - 1) { // i.e. we've finished iterating and it didn't break so it must be a line with only x's
               blockCell = i;
-              console.log( `blockCell = ${blockCell}` );
+              // console.log( `blockCell = ${blockCell}` );
               // remove simulation
               $('.xo').eq( i ).text('');
               $('.xo').eq( i ).removeClass('hidden');
@@ -121,7 +120,7 @@ const ai = {
 
         // if we haven't returned a blockCell, then remove simulation
         $('.xo').eq( i ).text('');
-        console.log( `Simulated X removed` );
+        // console.log( `Simulated X removed` );
         $('.xo').eq( i ).removeClass('hidden');
 
 
@@ -144,7 +143,7 @@ const ai = {
 
     if ( 2 < numCellsFilled ) { // if not the first turn
       choice = this.checkForPossWin();
-      console.log( `Choice after poss win f call: ${choice}` );
+      // console.log( `Choice after poss win f call: ${choice}` );
       if ( choice !== undefined ) return choice;
       }
 
@@ -173,33 +172,6 @@ const ai = {
     return choice;
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
